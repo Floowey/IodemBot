d783f271275aa132743c88d928196aaa8b1f4dfc
Flo <floowey@gmx.at>
2019-06-18 12:35:02 +0200

    Easy Has and Get Item

---
+++ IodemBot/Extensions/ItemListExtension.cs
@@ -0,0 +1,19 @@
+﻿using IodemBot.Modules.GoldenSunMechanics;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IodemBot.Extensions
+{
+    public static class ItemListExtension
+    {
+        public static Item GetItem(this List<Item> list, ItemCategory cat)
+        {
+            return list.Where(i => i.Category == cat).FirstOrDefault();
+        }
+
+        public static bool HasItem(this List<Item> list, ItemCategory cat)
+        {
+            return list.Any(i => i.Category == cat);
+        }
+    }
+}
