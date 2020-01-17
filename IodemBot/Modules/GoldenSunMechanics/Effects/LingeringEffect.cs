a546b0d33ecfe12603d1cd0cf332b23ebaf3bf62
Flo <floowey@gmx.at>
2020-01-17 14:36:18 +0100

    LingeringEffect Idea

---
+++ IodemBot/Modules/GoldenSunMechanics/Effects/LingeringEffect.cs
@@ -0,0 +1,26 @@
+﻿using IodemBot.Modules.ColossoBattles;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace IodemBot.Modules.GoldenSunMechanics
+{
+    public class LingeringEffect : Effect
+    {
+        public override List<string> Apply(ColossoFighter User, ColossoFighter Target)
+        {
+            if (coolDown > 0)
+            {
+                coolDown--;
+                return new List<string>();
+            }
+            else
+            {
+                return Effect.Apply(User, Target);
+            }
+        }
+
+        public override string Type { get; } = "Lingering";
+        [JsonProperty] private Effect Effect;
+        [JsonProperty] private int coolDown;
+    }
+}
