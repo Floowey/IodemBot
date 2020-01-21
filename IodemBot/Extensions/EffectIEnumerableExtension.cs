dd83f36e41edad397eda9756c2c33eba752783ea
Flo <floowey@gmx.at>
2020-01-21 14:18:03 +0100

    ApplyAll Function

---
+++ IodemBot/Extensions/EffectIEnumerableExtension.cs
@@ -0,0 +1,15 @@
+﻿using IodemBot.Modules.ColossoBattles;
+using IodemBot.Modules.GoldenSunMechanics;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IodemBot.Extensions
+{
+    public static class EffectIEnumerableExtension
+    {
+        public static IEnumerable<string> ApplyAll(this IEnumerable<Effect> effects, ColossoFighter User, ColossoFighter Target)
+        {
+            return effects.SelectMany(e => e.Apply(User, Target));
+        }
+    }
+}
