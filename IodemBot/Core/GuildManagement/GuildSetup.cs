cf71afcedbb3c5b495ea26c65bf9bedb2dc95599
Flo <floowey@gmx.at>
2020-01-21 14:16:27 +0100

    Add Guildsetups

---
+++ IodemBot/Core/GuildManagement/GuildSetup.cs
@@ -0,0 +1,32 @@
+﻿using Discord.WebSocket;
+using Newtonsoft.Json;
+
+namespace IodemBot.Core
+{
+    public class GuildSetup
+    {
+        public ulong GuildID;
+        public GuildSetupConfig guildConfig;
+        public bool sendWelcomeMessage;
+        public bool sendLeaveMessage;
+        public bool isTestServer;
+        public bool isUserServer;
+
+        [JsonIgnore] public SocketTextChannel MainChannel { get { return (SocketTextChannel)Global.Client.GetChannel(guildConfig.MainChannelID); } }
+        [JsonIgnore] public SocketTextChannel ModChannel { get { return (SocketTextChannel)Global.Client.GetChannel(guildConfig.ModChannelID); } }
+        [JsonIgnore] public SocketTextChannel CommandChannel { get { return (SocketTextChannel)Global.Client.GetChannel(guildConfig.CommandChannelID); } }
+        [JsonIgnore] public SocketTextChannel ColossoChannel { get { return (SocketTextChannel)Global.Client.GetChannel(guildConfig.ColossoChannelID); } }
+        [JsonIgnore] public SocketTextChannel TestCommandChannel { get { return (SocketTextChannel)Global.Client.GetChannel(guildConfig.TestCommandChannelID); } }
+        [JsonIgnore] public SocketTextChannel StreamChannel { get { return (SocketTextChannel)Global.Client.GetChannel(guildConfig.StreamChannelID); } }
+    }
+
+    public struct GuildSetupConfig
+    {
+        public ulong MainChannelID { get; set; }
+        public ulong ModChannelID { get; set; }
+        public ulong CommandChannelID { get; set; }
+        public ulong ColossoChannelID { get; set; }
+        public ulong TestCommandChannelID { get; set; }
+        public ulong StreamChannelID { get; set; }
+    }
+}
---
+++ IodemBot/Core/GuildManagement/GuildSetups.cs
@@ -0,0 +1,76 @@
+﻿using Discord;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IodemBot.Core
+{
+    public class GuildSetups
+    {
+        private static List<GuildSetup> guilds;
+        private static readonly string guildsFile = "Resources/Accounts/guilds.json";
+
+        static GuildSetups()
+        {
+            try
+            {
+                if (DataStorage.SaveExists(guildsFile))
+                {
+                    guilds = DataStorage.LoadListFromFile<GuildSetup>(guildsFile).ToList();
+                }
+                else
+                {
+                    guilds = new List<GuildSetup>();
+                    SaveGuilds();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        public static void SaveGuilds()
+        {
+            try
+            {
+                DataStorage.SaveUserAccounts(guilds, guildsFile);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        public static GuildSetup GetAccount(IGuild user)
+        {
+            return GetOrCreateGuild(user.Id);
+        }
+
+        private static GuildSetup GetOrCreateGuild(ulong id)
+        {
+            var result = from a in guilds
+                         where a.GuildID == id
+                         select a;
+
+            var account = result.FirstOrDefault();
+            if (account == null)
+            {
+                account = CreateGuild(id);
+            }
+
+            return account;
+        }
+
+        private static GuildSetup CreateGuild(ulong id)
+        {
+            var newAccount = new GuildSetup()
+            {
+                GuildID = id
+            };
+            guilds.Add(newAccount);
+            SaveGuilds();
+            return newAccount;
+        }
+    }
+}
