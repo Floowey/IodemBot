12a6c440b347788ec066d7bf9ce3bfdc961677e8
Flo <floowey@gmx.at>
2020-07-12 16:47:09 +0200

    Fix shell

--- IodemBot/shellscripts/selfupdate.sh
+++ IodemBot/shellscripts/selfupdate.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # selfupdate.sh
 # Self Update, this should be able to be called from within the bot itself
-killall IodemBot
+sudo systemctl stop IodemBotService.service
 cd ~/IodemBot/IodemBot/
 git pull
 dotnet publish -o /home/pi/bot/
 cd /home/pi/bot
-dotnet IodemBot.dll
+sudo systemctl restart IodemBotService.service
