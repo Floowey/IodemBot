1ecac1207c187afcead497e78607d0397939611a
Flo <floowey@gmx.at>
2020-08-03 14:53:57 +0200

    change auto update

--- IodemBot/shellscripts/selfupdate.sh
+++ IodemBot/shellscripts/selfupdate.sh
@@ -3,10 +3,10 @@
 # Self Update, this should be able to be called from within the bot itself
 echo "Start Updating"
 echo $PWD
-cd ~/IodemBot/IodemBot/ # Folder of Repository
+cd ../repo/IodemBot # Folder of Repository
 git pull
 echo "done pulling"
-dotnet publish -o /home/pi/bot/ # Folder of compiled program
+dotnet publish -o ../bin # Folder of compiled program
 echo "done publishing"
 sudo systemctl restart IodemBotService # Background service linked to /home/pi/bot/IodemBot.dll
 echo "process started"
