0dbf4169b0fe74bd724a3e8fa81796716ae65ebb
Flo <floowey@gmx.at>
2020-08-03 18:26:19 +0200

    Self Update fix

--- IodemBot/shellscripts/selfupdate.sh
+++ IodemBot/shellscripts/selfupdate.sh
@@ -8,6 +8,6 @@ echo "done pulling"
 dotnet publish -o ../../bin # Folder of compiled program
 echo "done publishing"
 
-SERVICE = ${1:-"MedoiBotService"}
+SERVICE=${1:-"MedoiBotService"}
 sudo systemctl restart "$SERVICE" # Background service linked to /home/pi/bot/IodemBot.dll
 echo "process started"
