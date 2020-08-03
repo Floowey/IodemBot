3fdf3bfbfd3d98c82229f50dd951f76f41ffc8b3
Flo <floowey@gmx.at>
2020-08-03 17:55:50 +0200

    selfupdate

--- IodemBot/shellscripts/selfupdate.sh
+++ IodemBot/shellscripts/selfupdate.sh
@@ -2,7 +2,7 @@
 # selfupdate.sh
 # Self Update, this should be able to be called from within the bot itself
 echo "Start Updating"
-cd ../IodemBot # Folder of Repository
+cd ../repo/IodemBot # Folder of Repository
 git pull
 echo "done pulling"
 dotnet publish -o ../../bin # Folder of compiled program
