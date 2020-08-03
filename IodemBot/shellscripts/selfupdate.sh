6884e0e1f7ca534f7de7659e3687107bfa442a85
Flo <floowey@gmx.at>
2020-08-03 18:14:58 +0200

    Bot specific self update

--- IodemBot/shellscripts/selfupdate.sh
+++ IodemBot/shellscripts/selfupdate.sh
@@ -7,5 +7,7 @@ git pull
 echo "done pulling"
 dotnet publish -o ../../bin # Folder of compiled program
 echo "done publishing"
-sudo systemctl restart IodemBotService # Background service linked to /home/pi/bot/IodemBot.dll
+
+SERVICE = ${1:?"MedoiBotService"}
+sudo systemctl restart "$SERVICE" # Background service linked to /home/pi/bot/IodemBot.dll
 echo "process started"
