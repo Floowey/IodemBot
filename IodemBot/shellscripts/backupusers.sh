f4ed235417a870031599a6d8b87d88bd25639e8a
Flo <floowey@gmx.at>
2020-08-06 13:28:03 +0200

    scripts

--- IodemBot/shellscripts/backupusers.sh
+++ IodemBot/shellscripts/backupusers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # backupusers.sh
 # Backup users to git, override everything
-cd /Resources/Accounts/
+cd Resources/Accounts/
 git commit -a -m "Backup Accounts"
 git pull -s recursive -X ours
 git push
