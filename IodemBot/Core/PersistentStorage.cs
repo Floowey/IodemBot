14610c2a0241a44c7b1c222476da481ecfea3568
Flo <floowey@gmx.at>
2020-08-19 21:16:50 +0200

    Try adding LiteDB

---
+++ IodemBot/Core/PersistentStorage.cs
@@ -0,0 +1,79 @@
+﻿// This file is part of Miunie.
+//
+//  Miunie is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Miunie is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Miunie. If not, see <https://www.gnu.org/licenses/>.
+
+using LiteDB;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace IodemBot.Core
+{
+    public class PersistentStorage
+    {
+        private readonly string _dbFileName;
+
+        public PersistentStorage()
+        {
+            _dbFileName = Path.Combine("Resources","", "Miunie.db");
+        }
+
+        public IEnumerable<T> RestoreMany<T>(Expression<Func<T, bool>> predicate)
+        {
+            using var db = new LiteDatabase(_dbFileName);
+            var collection = db.GetCollection<T>();
+            return collection.Find(predicate);
+        }
+
+        public IEnumerable<T> RestoreAll<T>()
+        {
+            using var db = new LiteDatabase(_dbFileName);
+            var collection = db.GetCollection<T>();
+            return collection.FindAll();
+        }
+
+        public T RestoreSingle<T>(Expression<Func<T, bool>> predicate)
+            => RestoreMany(predicate).FirstOrDefault();
+
+        public bool Exists<T>(Expression<Func<T, bool>> predicate)
+        {
+            using var db = new LiteDatabase(_dbFileName);
+            var collection = db.GetCollection<T>();
+            return collection.Exists(predicate);
+        }
+
+        public void Store<T>(T item)
+        {
+            using var db = new LiteDatabase(_dbFileName);
+            var collection = db.GetCollection<T>();
+            _ = collection.Insert(item);
+        }
+
+        public void Update<T>(T item)
+        {
+            using var db = new LiteDatabase(_dbFileName);
+            var collection = db.GetCollection<T>();
+            _ = collection.Update(item);
+        }
+
+        public void Remove<T>(Expression<Func<T, bool>> predicate)
+        {
+            using var db = new LiteDatabase(_dbFileName);
+            var collection = db.GetCollection<T>();
+            _ = collection.DeleteMany(predicate);
+        }
+    }
+}
---
+++ IodemBot/Core/UserManagement/UserAccountProvider.cs
@@ -0,0 +1,59 @@
+﻿
+using System.Collections.Generic;
+
+namespace IodemBot.Core.UserManagement
+{
+    public static class MiunieUserProvider
+    {
+        private static PersistentStorage _persistentStorage;
+
+        static MiunieUserProvider()
+        {
+            _persistentStorage = new PersistentStorage();
+        }
+
+        public static UserAccount GetById(ulong userId)
+        {
+            var user = _persistentStorage.RestoreSingle<UserAccount>(u => u.ID == userId);
+            return EnsureExists(user, userId);
+        }
+
+        public static void StoreUser(UserAccount user)
+        {
+            if (_persistentStorage.Exists<UserAccount>(u => u.ID == user.ID))
+            {
+                _persistentStorage.Update(user);
+            }
+            else
+            {
+                _persistentStorage.Store(user);
+            }
+        }
+
+        public static void RemoveUser(UserAccount user)
+        {
+            if (_persistentStorage.Exists<UserAccount>(u => u.ID == user.ID))
+            {
+                _persistentStorage.Remove<UserAccount>(u => u.ID == user.ID);
+            }
+        }
+
+        public static IEnumerable<UserAccount> GetAllUsers()
+            => _persistentStorage.RestoreAll<UserAccount>();
+
+        private static UserAccount EnsureExists(UserAccount user, ulong userId)
+        {
+            if (user is null)
+            {
+                user = new UserAccount
+                {
+                    ID=userId
+
+                };
+                StoreUser(user);
+            }
+
+            return user;
+        }
+    }
+}
