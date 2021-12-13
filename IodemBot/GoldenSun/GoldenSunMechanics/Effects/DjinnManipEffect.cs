cdb3634bacee9d64b24f242638933ad1490493e0
Flo <floowey@gmx.at>
2021-12-13 14:31:05 +0100

    Funny stuff with Djinn Manip. Hopefully for the better.

--- IodemBot/GoldenSun/GoldenSunMechanics/Effects/DjinnManipEffect.cs
+++ IodemBot/GoldenSun/GoldenSunMechanics/Effects/DjinnManipEffect.cs
@@ -13,8 +13,8 @@ namespace IodemBot.Modules.GoldenSunMechanics
     {
         public override string Type => "DjinnManip";
         [JsonProperty] private int nDjinn { get; set; } = 1;
-        [JsonProperty] private DjinnState FromState = DjinnState.Set;
-        [JsonProperty] private DjinnState ToState = DjinnState.Standby;
+        [JsonProperty] private DjinnState FromState { get; set; } = DjinnState.Set;
+        [JsonProperty] private DjinnState ToState { get; set; } = DjinnState.Standby;
 
         public DjinnManipEffect()
         {
@@ -34,19 +34,21 @@ namespace IodemBot.Modules.GoldenSunMechanics
                 {
                     case DjinnState.Set:
                         djinn.OnStandby = false;
+                        target.Moves.OfType<Djinn>().Where(d => d.CoolDown > djinn.CoolDown).ToList().ForEach(d => d.CoolDown--);
                         djinn.CoolDown = 0;
                         log.Add($"{djinn.Name} is set to {target.Name}");
                         break;
 
                     case DjinnState.Standby:
                         djinn.OnStandby = true;
+                        djinn.CoolDown = Math.Max(2, user.Moves.OfType<Djinn>().Max(d => d.CoolDown) + 1);
                         djinn.Position = target.Party.SelectMany(p => p.Moves.OfType<Djinn>()).Max(d => d.Position) + 1;
-                        log.Add($"{target.Name}'s {djinn.Name} is on standby.");
+                        log.Add($"{target.Name}'s {djinn.Name} was set to standby.");
                         break;
 
                     case DjinnState.Recovery:
                         djinn.OnStandby = false;
-                        djinn.CoolDown = Math.Max(2, target.Moves.OfType<Djinn>().Max(d => d.CoolDown) + 1);
+                        djinn.CoolDown = FromState == DjinnState.Set ? Math.Max(2, target.Moves.OfType<Djinn>().Max(d => d.CoolDown) + 1) : djinn.CoolDown;
                         djinn.Position = target.Party.SelectMany(p => p.Moves.OfType<Djinn>()).Max(d => d.Position) + 1;
                         log.Add($"{target.Name}'s {djinn.Name} was drained ");
                         break;
