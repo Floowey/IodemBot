7fd9d42f4c6d360c4dee5011ddf32f3b33380f0b
Flo <floowey@gmx.at>
2023-04-08 11:35:21 +0200

    Add Unit Tests

---
+++ IodemBotTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace IodemBotTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestWorks()
+        {
+            Assert.Equal(1, 1);
+        }
+    }
+}
